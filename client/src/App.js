e869a3843d76966a19c7a26246c9ca7c152e9f6c
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-28 16:38:00 +0800

    removed testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "antd/dist/antd.css";
+import { Row, Col, Input } from "antd";
+
+import FormComponent from "./Component/FormComponent";
+import TableComponent from "./Component/TableComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <Row>
+        <Col span={24}>
+          <h1>URL Shortening Tool</h1>
+        </Col>
+      </Row>
+      <Row className="main-form">
+        <Col span={24}>
+          <FormComponent />
+          <Row style={{ padding: "15px 100px" }}>
+            <Col span={24}>
+              <Input
+                style={{ textAlign: "center" }}
+                placeholder="new url is placed here"
+                bordered={false}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <TableComponent />
+        </Col>
+      </Row>
     </div>
   );
 }
