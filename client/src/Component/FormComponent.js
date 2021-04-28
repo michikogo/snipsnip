152217767e6369e07dce030c251df3210c5e1c27
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-28 16:39:00 +0800

    created layout

---
+++ client/src/Component/FormComponent.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Form, Input, Button, Avatar } from "antd";
+
+const FormComponent = () => {
+  const formRef = React.createRef();
+
+  const [fullURL, setFullURL] = useState("");
+
+  const handleSubmit = (value) => {
+    const rand = 1 + Math.random() * (100 - 1);
+    const shortURL = Math.floor(rand);
+    const newData = { fullURL, shortURL };
+    console.log(newData);
+
+    formRef.current.resetFields();
+    fetch("http://localhost:3001/insert", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newData),
+    })
+      .then(() => {
+        console.log("Insert Data");
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+  return (
+    <Form
+      name="customized_form_controls"
+      layout="inline"
+      style={{ justifyContent: "center" }}
+      // onSubmit with ref to clear form
+      onFinish={handleSubmit}
+      ref={formRef}
+    >
+      {/* URL Link */}
+      <Form.Item
+        name="fullURL"
+        rules={[
+          {
+            required: true,
+            message: "Missing URL ",
+          },
+        ]}
+      >
+        <Input
+          label="fullURL"
+          value={fullURL}
+          onChange={(e) => setFullURL(e.target.value)}
+          style={{ width: "55vh", height: "5vh" }}
+        />
+      </Form.Item>
+      {/* SUBMIT BUTTON */}
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          style={{ width: "9vh", height: "5vh" }}
+        >
+          <Avatar
+            shape="square"
+            size={24}
+            src={require(`../Assets/paper-plane.png`).default}
+          />
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default FormComponent;
---
+++ client/src/Component/TableComponent.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Table, Divider } from "antd";
+
+const TableComponent = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+    },
+  ];
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+    },
+    {
+      key: "4",
+      name: "Disabled User",
+      age: 99,
+      address: "Sidney No. 1 Lake Park",
+    },
+  ]; // rowSelection object indicates the need for row selection
+
+  return (
+    <div>
+      <Table pagination={false} columns={columns} dataSource={data} />
+    </div>
+  );
+};
+
+export default TableComponent;
