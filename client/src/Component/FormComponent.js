7740e4288638240fa26eea279e7ad50e03a6f7c1
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-29 23:51:56 +0800

    fixed layout and connected to backend

--- client/src/Component/FormComponent.js
+++ client/src/Component/FormComponent.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Avatar } from "antd";
+import { Row, Col, Form, Input, Button } from "antd";
 
 const FormComponent = () => {
-  const formRef = React.createRef();
-
   const [fullURL, setFullURL] = useState("");
+  const [shortURL, setShortURL] = useState("");
+  const [showNewURL, setShowNewURL] = useState(false);
+  const [finalURL, setFinalURL] = useState("");
 
-  const handleSubmit = (value) => {
-    const rand = 1 + Math.random() * (100 - 1);
-    const shortURL = Math.floor(rand);
-    const newData = { fullURL, shortURL };
-    // console.log(newData);
+  const handleSubmit = () => {
+    // check if custom url is made
+    var tempShortURL;
+    if (shortURL === "") {
+      const rand = 1 + Math.random() * (100000 - 1000);
+      tempShortURL = `https://www.snipsnip.com/${Math.floor(rand)}`;
+    } else {
+      tempShortURL = `https://www.snipsnip.com/${shortURL}`;
+    }
+    const newData = { fullURL, shortURL: tempShortURL, click: 0 };
+    console.log(newData);
 
-    formRef.current.resetFields();
+    // Place in db
     fetch("http://localhost:3001/insert", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -20,6 +27,8 @@ const FormComponent = () => {
     })
       .then(() => {
         console.log("Insert Data");
+        setShowNewURL(true);
+        setFinalURL(tempShortURL);
       })
       .catch((err) => {
         throw err;
@@ -27,46 +36,66 @@ const FormComponent = () => {
   };
 
   return (
-    <Form
-      name="customized_form_controls"
-      layout="inline"
-      style={{ justifyContent: "center" }}
-      // onSubmit with ref to clear form
-      onFinish={handleSubmit}
-      ref={formRef}
-    >
-      {/* URL Link */}
-      <Form.Item
-        name="fullURL"
-        rules={[
-          {
-            required: true,
-            message: "Missing URL ",
-          },
-        ]}
+    <div>
+      <Form
+        name="customized_form_controls"
+        // layout="inline"
+        style={{ justifyContent: "center" }}
+        onFinish={handleSubmit}
       >
-        <Input
-          label="fullURL"
-          value={fullURL}
-          onChange={(e) => setFullURL(e.target.value)}
-          style={{ width: "55vh", height: "5vh" }}
-        />
-      </Form.Item>
-      {/* SUBMIT BUTTON */}
-      <Form.Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          style={{ width: "9vh", height: "5vh" }}
+        <Form.Item
+          name="fullURL"
+          rules={[{ required: true, message: "Not a valid URL" }]}
         >
-          <Avatar
-            shape="square"
-            size={24}
-            src={require(`../Assets/paper-plane.png`).default}
+          <Input
+            value={fullURL}
+            onChange={(e) => setFullURL(e.target.value)}
+            placeholder="Original URL"
+            style={{ width: "55vh", height: "5vh" }}
           />
-        </Button>
-      </Form.Item>
-    </Form>
+        </Form.Item>
+        <Form.Item name="shortURL">
+          <Input
+            value={shortURL}
+            onChange={(e) => setShortURL(e.target.value)}
+            placeholder="Custom URL Name (Optional)"
+            style={{ width: "55vh", height: "5vh" }}
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            style={{ alignItems: "right" }}
+          >
+            Create
+          </Button>
+        </Form.Item>
+      </Form>
+      {showNewURL && (
+        <div>
+          <Row>
+            <Col span={24}>New URL</Col>
+          </Row>
+          <Row style={{ margin: "1vh 14vh" }}>
+            <Col
+              span={24}
+              style={{
+                width: "50%",
+                padding: "5px 15px",
+                border: "1px solid #d9d9d9",
+                borderRadius: "2px",
+                cursor: "pointer",
+              }}
+            >
+              {finalURL}
+            </Col>
+          </Row>
+        </div>
+      )}
+    </div>
   );
 };
 
